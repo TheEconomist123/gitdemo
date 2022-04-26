d85ae1fc28103653b1703b7a2effb1ff937d77ba
夜幕降临 <2248869976@qq.com>
2022-04-26 20:10:02 +0800

    111

---
+++ git-demo/src/main/java/com/itheima/GitHello.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+/**
+ * Hello world!
+ */
+public class GitHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
